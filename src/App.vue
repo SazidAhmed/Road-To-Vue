12d1b02c2c6ceb85e39514297bc4669f6dcaab74
SazidAhmed <sazidahmed.official@gmail.com>
2021-03-29 22:58:32 +0600

    Form Data binding

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,20 @@
 <template>
-  <h1>{{title}}</h1>
-  <!-- create modal -->
-  <teleport to=".modals" v-if="showModal">
-    <Modal>
-      <!-- default slot -->
-      <h1>Create Data</h1>
-      <p>Insert Data Here</p>
-      <!-- name slot -->
-      <template v-slot:links>
-          <a href="#" @click="submitForm">Submit</a>
-          <a href="#" @click="toggleModal">Close</a>
-      </template>
-      <!-- name slot -->
-    </Modal>
-  </teleport>
-  <!-- delete modal -->
-  <div v-if="showModalTwo">
-    <Modal>
-      <!-- default slot -->
-      <h1>Delelte Data</h1>
-      <p>Are You Sure ?</p>
-      <!-- name slot -->
-      <template v-slot:links>
-          <a href="#" @click="confimDelete">Confirm</a>
-          <a href="#" @click="toggleModalTwo">Close</a>
-      </template>
-      <!-- name slot -->
-    </Modal>
-  </div>
-  <button @click="toggleModal">Add New</button>
-  <button @click="toggleModalTwo">Delete</button>
+  <SignupForm />
 </template>
 
 <script>
 import Modal from './components/Modal.vue'
-
+import SignupForm from './components/SignupForm'
 export default {
   name: 'App',
-  components:{ Modal },
+  components:{ Modal,  SignupForm },
   data(){
     return{
-      showModal:false,
-      showModalTwo:false,
-      title:'Vue app',
+     
     }
   },
   methods:{
-    toggleModal(){
-      this.showModal = !this.showModal
-    },
-    toggleModalTwo(){
-      this.showModalTwo = !this.showModalTwo
-    },
-    submitForm(){
-      alert("form has been submitted !")
-      this.showModal = false;
-    },
-    confimDelete(){
-      alert("Data has been Deleted !")
-      this.showModalTwo = false;
-    }
+    
   }
 }
 </script>
@@ -72,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/global.css'
 
 createApp(App).mount('#app')
